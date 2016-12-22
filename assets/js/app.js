13a86bc455a992b952b479908ec0520deba388a4
Tania A <taniaaallen@gmail.com>
2016-12-22 18:39:54 -0500

    moved folders into assets folder, added smooth scroll jquery to page and fixed nav alignment

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
 console.log('this is the js file!');
 
-$('#scroll').on('click', function(event) {
+
+$('a[href^="#"]').on('click', function(event) {
 
     var target = $(this.getAttribute('href'));
-		console.log(target);
-
+    
     if( target.length ) {
         event.preventDefault();
         $('html, body').stop().animate({
